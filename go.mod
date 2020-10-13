25f25e7295d28850118f639a7f2647e923d1910e
Thomas Mitchell <klaue@live.com>
2020-10-13 12:28:44 -0400

    authenticate to concourse

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	gopkg.in/square/go-jose.v2 v2.3.1
 	gopkg.in/yaml.v2 v2.3.0
 )
 
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
+	"encoding/base64"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -13,6 +17,8 @@ import (
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/thomasmitchell/prism/config"
+	"golang.org/x/oauth2"
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 func main() {
@@ -22,14 +28,25 @@ func main() {
 	if err != nil {
 		bailWith("Error loading config: %s", err)
 	}
+	clientTransport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: cfg.Concourse.InsecureSkipVerify,
+		},
+	}
+
+	authClient := &http.Client{Transport: clientTransport}
 
 	concourseClient := concourse.NewClient(
 		cfg.Concourse.URL,
 		&http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: cfg.Concourse.InsecureSkipVerify,
-				},
+			Transport: &oauth2.Transport{
+				Source: oauth2.ReuseTokenSource(nil, &concourseAuth{
+					client:   authClient,
+					url:      cfg.Concourse.URL,
+					username: cfg.Concourse.Auth.Username,
+					password: cfg.Concourse.Auth.Password,
+				}),
+				Base: clientTransport,
 			},
 		},
 		false,
@@ -97,4 +114,51 @@ func logReq(uuid uuid.UUID, f string, args ...interface{}) {
 
 func fLog(w io.Writer, f string, args ...interface{}) {
 	fmt.Printf("%s: "+f+"\n", append([]interface{}{time.Now().Format(time.RFC3339)}, args...)...)
+}
+
+type concourseAuth struct {
+	client   *http.Client
+	url      string
+	username string
+	password string
+}
+
+func (c concourseAuth) Token() (*oauth2.Token, error) {
+	oauth2Config := oauth2.Config{
+		ClientID:     "prism",
+		ClientSecret: "Zmx5",
+		Endpoint:     oauth2.Endpoint{TokenURL: c.url + "/sky/issuer/token"},
+		Scopes:       []string{"openid", "profile", "email", "federated:id", "groups"},
+	}
+
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, c.client)
+
+	token, err := oauth2Config.PasswordCredentialsToken(ctx, c.username, c.password)
+	if err != nil {
+		log("error fetching oauth2 token: %s", err)
+		return nil, err
+	}
+	expiry, err := c.parseTokenExpiry(token.AccessToken)
+	if err != nil {
+		log("error parsing token expiry: %s", err)
+		return nil, fmt.Errorf("error parsing token expiry: %s", err)
+	}
+
+	return &oauth2.Token{
+		TokenType:   token.TokenType,
+		AccessToken: token.AccessToken,
+		Expiry:      expiry,
+	}, nil
+}
+
+func (c concourseAuth) parseTokenExpiry(token string) (time.Time, error) {
+	raw, err := base64.RawStdEncoding.DecodeString(token)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if len(raw) != 28 {
+		return time.Time{}, errors.New("invalid access token length")
+	}
+	expiry := jwt.NumericDate(binary.LittleEndian.Uint64(raw[20:]))
+	return expiry.Time(), nil
 }
